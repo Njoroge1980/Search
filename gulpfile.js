644f5da382650e9e93e16a639cfe7cad0a687c25
Njoroge Mungai <njorogemungai1980@gmail.com>
2017-09-08 17:30:42 +0300

    add a task to concat the interface files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var utilities = require('gulp-util');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var jshint = require('gulp-jshint');
 
-gulp.task('jsBrowserify', function() {
-  return browserify({
-      entries: ['./js/search-interface.js']
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+});
+
+gulp.task('jsBrowserify', ['concatInterface']function() {
+    return browserify({
+      entries: ['./tmp/allConcat.js']
     })
-    .bundle()
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('./build/js'));
+  })
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./build/js'));
 });
